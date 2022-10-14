3133145b671fb91b54d10c3ecdeb98fa2c4f6300
DJCAYZ <charlesaaron07@gmail.com>
2022-10-14 17:29:35 +0800

    Add command handling
    Per-file command handling is added. Command registration is done during
    the bot's initialization phase instead of a separate script.
    
    The debug module is also added as a dependency and will act as a verbose
    logger. To enable logging, just set the DEBUG environment variable to
    ' xingqiu:<filename> ' to only enable logging for a single file or
    ' xingqiu:* ' to enable logging for the whole project. Refer to debug
    module's documentation in npm for more details.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,76 @@
 require('dotenv').config();
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST, Routes, Client, GatewayIntentBits, Collection } = require('discord.js');
+const debug = require('debug')('xingqiu:index');
 
 class XingqiuBot extends Client {
   constructor() {
     super({ intents: [GatewayIntentBits.Guilds] });
+
+    this.commands = new Collection();
+
+    debug('Initializing Xingqiu Bot...');
     this.init();
   }
 
   init() {
-    console.log('Initializing bot...');
+    debug('Registering application commands...');
+    const cmdData = [];
+    const cmdPath = path.join(__dirname, 'commands');
+    const cmdFiles = fs.readdirSync(cmdPath).filter(file => file.endsWith('.js'));
+
+    cmdFiles.forEach(file => {
+      const filePath = path.join(cmdPath, file);
+      const cmd = require(filePath);
+      this.commands.set(cmd.data.name, cmd);
+      cmdData.push(cmd.data.toJSON());
+    });
+
+    const rest = new REST({ version: '10'}).setToken(process.env.TOKEN);
+    // If there is a defined TESTGUILDID in .env,
+    // commands will be registered as guild-only
+    // else, they will be registered globally
+    const clientID = process.env.CLIENTID;
+    const guildID = process.env.TESTGUILDID;
+
+    (async () => {
+      try {
+        if (process.env.TESTGUILDID) {
+          const data = await rest.put(Routes.applicationGuildCommands(clientID, guildID), { body: cmdData });
+          debug(`Registration of ${data.length} application commands successful.`);
+        } else {
+          const data = await rest.put(Routes.applicationCommands(clientID), { body: cmdData });
+          debug(`Registration of ${data.length} application commands successful.`);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+
+
+    debug('Registering event handlers...');
+    this.on('interactionCreate', async interaction => {
+      if (!interaction.isChatInputCommand()) return;
+
+      const cmd = interaction.client.commands.get(interaction.commandName);
+
+      if (!cmd) return;
+
+      try {
+        await cmd.execute(interaction);
+      } catch (error) {
+        await interaction.reply({ content: 'There was an error while executing this command.', ephemeral: true });
+      }
+    });
   }
 
   start() {
-    console.log('Xingqiu Bot starting...');
+    debug('Xingqiu Bot starting...');
     this.login(process.env.TOKEN);
     this.once('ready', () => {
-      console.log('Xingqiu Bot started.');
+      debug('Xingqiu Bot started.');
       process.env.TOKEN = '';
     });
   }
